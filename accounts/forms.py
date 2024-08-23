4966b3b4d39006a5716e2b70fb3013ee6fbe3917
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-08-23 18:09:10 +0000

    Feat: Set email as a required form input.

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,9 +4,13 @@ from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].required = True
+
     class Meta:
         model = CustomUser
-        fields = ("username", "email")
+        fields = ('username', 'first_name', 'last_name', 'email')
 
 
 class CustomUserChangeForm(UserChangeForm):
