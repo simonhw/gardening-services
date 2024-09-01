40be5f766ad1addf76b840e44f6050706d387eab
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-09-01 18:39:23 +0000

    Feat: Add hierarchy links to services pages.

--- services/views.py
+++ services/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Service
+from .models import Service, Category
 
 
 def all_services(request):
@@ -8,9 +8,20 @@ def all_services(request):
     '''
 
     services = Service.objects.all()
+    categories = None
+    category_heading = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            services = services.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+            category_heading = True
 
     context = {
         'services': services,
+        'current_categories': categories,
+        'category_heading': category_heading,
     }
 
     return render(request, "services/services.html", context)
