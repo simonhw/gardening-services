3c7bc5f0d6305ee44aadd335e143aa03bb38eb91
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-09-06 17:42:26 +0000

    Refactor: Show or hide form inputs based on model fields.

--- static/js/service_form.js
+++ static/js/service_form.js
@@ -9,30 +9,24 @@ let serviceNoun = document.getElementsByClassName('service-name');
 
 // Show relevant fields for Grass Cutting
 if ( serviceName.includes('grass') ) {
-    acreInput.removeAttribute('class', 'd-none');
-
-    sizeInput.setAttribute('class', 'd-none');
     numberInput.setAttribute('class', 'd-none');
-    treeInput.setAttribute('class', 'd-none');
-    weedInput.setAttribute('class', 'd-none');
 }
 
 // Show relevant fields for Weeding and update based on surface type
 if ( serviceName.includes('weed') ) {
-    weedInput.removeAttribute('class', 'd-none');
-    
     sizeInput.setAttribute('class', 'd-none');
     numberInput.setAttribute('class', 'd-none');
-    treeInput.setAttribute('class', 'd-none');
-    acreInput.setAttribute('class', 'd-none');
     
     weedInput.addEventListener('change', function () {
         let weedOption = document.getElementById('weedOption');
         console.log("weedOption.value:", weedOption.value);
         if ( weedOption.value == 'bed' ) {
+            updateServiceName(serviceNoun, "Bed/Planter");
+            numberInput.removeAttribute('class', 'd-none');
             sizeInput.removeAttribute('class', 'd-none');
 
         } else {
+            numberInput.setAttribute('class', 'd-none');
             sizeInput.setAttribute('class', 'd-none');
         }
     });
@@ -40,42 +34,23 @@ if ( serviceName.includes('weed') ) {
 
 // Show relevant fields for Tree Cutting and Pruning
 if ( serviceName.includes('tree cutting') ) {
-    acreInput.setAttribute('class', 'd-none');
-
     updateServiceName(serviceNoun, "Tree");
-
-    sizeInput.removeAttribute('class', 'd-none');
-    numberInput.removeAttribute('class', 'd-none');
-    treeInput.removeAttribute('class', 'd-none');
-    weedInput.setAttribute('class', 'd-none');
 }
 
 // Show relevant fields for Hedge Cutting and Flowerbed Care
 if ( serviceName.includes('hedge') || serviceName.includes('flower') ) {
-    acreInput.setAttribute('class', 'd-none');
-
     if ( serviceName.includes('hedge') ) {
         updateServiceName(serviceNoun, "Hedge");
     } else {
         updateServiceName(serviceNoun, "Flowerbed");
     }
-
-    sizeInput.removeAttribute('class', 'd-none');
     numberInput.removeAttribute('class', 'd-none');
-    treeInput.setAttribute('class', 'd-none');
-    weedInput.setAttribute('class', 'd-none');
 }
 
 // Show relevant fields for Tree Stump Removal
 if ( serviceName.includes('stump') ) {
     numberInput.removeAttribute('class', 'd-none');
-
     updateServiceName(serviceNoun, "Stump");
-
-    acreInput.setAttribute('class', 'd-none');
-    sizeInput.setAttribute('class', 'd-none');
-    treeInput.setAttribute('class', 'd-none');
-    weedInput.setAttribute('class', 'd-none');
 }
 
 /**
