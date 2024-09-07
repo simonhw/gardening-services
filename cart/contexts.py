6bf02585c7708580a8c49f9a6a4da05e1c5f1353
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-09-07 16:32:20 +0000

    Feat: Add inita  logic to handle adding different weed services to cart.

--- cart/contexts.py
+++ cart/contexts.py
@@ -13,15 +13,66 @@ def cart_contents(request):
     item_count = 0
     cart = request.session.get('cart', {})
 
-    for item_id, number in cart.items():
-        service = get_object_or_404(Service, pk=item_id)
-        total += number * service.unit_price
-        item_count += number
-        cart_items.append({
-            'item_id': item_id,
-            'number': number,
-            'service': service,
-        })
+    for item_id, item_data in cart.items():
+        if isinstance(item_data, int):
+            service = get_object_or_404(Service, pk=item_id)
+            total += item_data * service.unit_price
+            item_count += item_data
+            cart_items.append({
+                'item_id': item_id,
+                'number': item_data,
+                'service': service,
+            })
+        else:
+            service = get_object_or_404(Service, pk=item_id)
+            if 'surfaces' in item_data.keys():
+
+                print("item_data['surfaces'].keys() is", item_data['surfaces'].keys()) # Debugging comment
+                # print("item_data['surfaces']['drive'] is", item_data['surfaces']['drive']) # Debugging comment
+
+                for surface in item_data['surfaces'].items():
+                    print('For Loop 1:')
+                    print('surface:', surface)
+
+                    if 'drive' in item_data['surfaces'].keys():
+
+                        print('drive is in the above!') # Debugging comment
+
+                        for surface, number in item_data['surfaces'].items():
+                            if surface == 'drive':
+                                print('For Loop 2:')
+                                print('surface:', surface)
+                                print('number:', number)
+                                total += number * service.unit_price
+                                item_count += number
+                                cart_items.append({
+                                    'item_id': item_id,
+                                    'number': item_data,
+                                    'service': service,
+                                    'surface': surface,
+                                })
+                            else:
+                                break
+                    if 'bed' in item_data['surfaces'].keys():
+                        for size, number in item_data['surfaces']['bed']['sizes'].items():
+                            total += number * service.unit_price
+                            item_count += number
+                            cart_items.append({
+                                'item_id': item_id,
+                                'number': item_data,
+                                'service': service,
+                                'size': size,
+                            })
+            else:
+                for size, number in item_data['sizes'].items():
+                    total += number * service.unit_price
+                    item_count += number
+                    cart_items.append({
+                        'item_id': item_id,
+                        'number': item_data,
+                        'service': service,
+                        'size': size,
+                    })
     context = {
         'cart_items': cart_items,
         'total': total,
--- cart/views.py
+++ cart/views.py
@@ -15,34 +15,78 @@ def add_to_cart(request, item_id):
     Add the service to the shopping cart with its relevant options
     """
 
-    size = request.POST.get('size')
     number = int(request.POST.get('number'))
-    acres = request.POST.get('acres')
-    tree = request.POST.get('tree')
-    surface = request.POST.get('surface')
     redirect_url = request.POST.get('redirect_url')
 
+    size = None
+    acres = None
+    tree = None
+    surface = None
+    
+    if 'size' in request.POST:
+        size = request.POST.get('size')
+    
+    if 'tree' in request.POST:
+        tree = request.POST.get('tree')
+    
+    if 'surface' in request.POST:
+        surface = request.POST.get('surface')
+
     cart = request.session.get('cart', {})
-    print('cart is', cart)
-    # print('cart.keys() is', cart.keys())
-    if item_id in list(cart.keys()):
-        # print('item_id is', item_id)
-        cart[item_id] += number
-        # if item_id == '1':
-            # cart[item_id] += acres
-        # cart[item_id] += size
-        # cart[item_id] += acres
-        # cart[item_id] += tree
-        # cart[item_id] += surface
-        # print('cart[item_id] is', cart[item_id])
+    print('cart is', cart)  # Debugging comment
+    print('size is', size)  # Debugging comment
+    print('surface is', surface) # Debugging comment
+
+    if surface:
+        if surface == 'bed':
+            if item_id in list(cart.keys()):
+                if surface in cart[item_id]['surfaces'].keys():
+                # Add a bed/planter to a cart already containing a bed/planter
+                    if size in cart[item_id]['surfaces'][surface]['sizes'].keys():
+                        cart[item_id]['surfaces'][surface]['sizes'][size] += number
+                    else:
+                        cart[item_id]['surfaces'][surface]['sizes'][size] = number
+                else:
+                    cart[item_id]['surfaces'][surface] = {'sizes': {size: number}}
+
+            else:
+                # Add a bed/planter to an empty cart
+                cart[item_id] = {'surfaces': {surface: {'sizes': {size: number}}}}
+        else:
+            if item_id in list(cart.keys()):
+                # Add a driveway to a cart containing at least 1 bed/planter
+                # if surface in list(cart[item_id]['surfaces'].keys())
+                # Add a driveway to a cart containing a driveway
+                print('-------------------------') # Debugging comment
+                print('cart[item_id] is', cart[item_id]) # Debugging comment
+                print("cart[item_id]['surfaces'] is", cart[item_id]['surfaces']) # Debugging comment
+                print("cart[item_id]['surfaces'][surface] is", cart[item_id]['surfaces'][surface]) # Debugging comment
+                print('-------------------------') # Debugging comment
+                cart[item_id]['surfaces'][surface] += number
+            else:
+                # Add a driveway to empty cart
+                cart[item_id] = {'surfaces': {surface: number}}
+                print('-------------------------') # Debugging comment
+                print('cart[item_id] is', cart[item_id]) # Debugging comment
+                print("cart[item_id]['surfaces'] is", cart[item_id]['surfaces']) # Debugging comment
+                print("cart[item_id]['surfaces'][surface] is", cart[item_id]['surfaces'][surface]) # Debugging comment
+                print('-------------------------') # Debugging comment
+        
+    # This handles both the acres option and the normal sizes option
+    elif size:
+        if item_id in list(cart.keys()):
+            if size in cart[item_id]['sizes'].keys():
+                cart[item_id]['sizes'][size] += number
+            else:
+                cart[item_id]['sizes'][size] = number
+        else:
+            cart[item_id] = {'sizes': {size: number}}
     else:
-        print('item_id is', item_id)
-        # cart[item_id] = {acres, number}
-        cart[item_id] = number
-        # if item_id == '1':
-            # cart[item_id] += acres
-        # print('cart[item_id] is', cart[item_id])
+        if item_id in list(cart.keys()):
+            cart[item_id] += number
+        else:
+            cart[item_id] = number
     
     request.session['cart'] = cart
-    print('cart is', cart)
+    print('cart is', cart) # Debugging comment
     return redirect(redirect_url)
