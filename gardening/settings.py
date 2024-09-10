a3f418838df061bd5b2ded024008112090b18fdf
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-09-10 21:34:32 +0000

    Feat: Add crispy forms settings and heading to checkout page.

--- gardening/settings.py
+++ gardening/settings.py
@@ -75,6 +75,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'gardening.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -88,6 +90,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
