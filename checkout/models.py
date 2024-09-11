aa455fbbc19a70c3ca899e9c118a7b696301aaa3
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-09-11 10:32:53 +0000

    Fix: Set Eircode field to required.

--- checkout/models.py
+++ checkout/models.py
@@ -23,7 +23,7 @@ class Order(models.Model):
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
-    county = models.CharField(max_length=80, null=False, blank=True)
+    county = models.CharField(max_length=80, null=False, blank=False)
     eircode = models.CharField(max_length=20, null=False, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     grand_total = models.DecimalField(
