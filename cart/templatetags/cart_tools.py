5ed46ccb8a8c7c9b23582d5e25a6143ba9872d37
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-09-11 14:50:59 +0000

    Feat: Add cart summary to checkout page.

---
+++ cart/templatetags/cart_tools.py
@@ -0,0 +1,12 @@
+from django import template
+
+
+register = template.Library()
+
+@register.filter(name='calc_subtotal')
+def calc_subtotal(unit_price, number):
+    """
+    Take in the unit price and numer of service and return the service.
+    """
+
+    return unit_price * number
