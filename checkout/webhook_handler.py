8eaf9b09ed3941709f56bc5fd76884f263047c65
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-09-17 16:56:22 +0000

    Feat: Update webhook handler to handle accounts.

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # from django.conf import settings
 from .models import Order, OrderLineItem
 from services.models import Service
-# from accounts.models import CustomUser
+from accounts.models import UserAccount
 
 import stripe
 
@@ -71,19 +71,18 @@ class StripeWH_Handler:
             if value == '':
                 shipping_details.address[field] = None
 
-#         profile = None
-#         username = intent.metadata.username
-#         if username != 'AnonymousUser':
-#             profile = UserProfile.objects.get(user__username=username)
-#             if save_info:
-#                 profile.default_phone_number = shipping_details.phone
-#                 profile.default_country = shipping_details.address.country
-#                 profile.default_postcode = shipping_details.address.postal_code
-#                 profile.default_town_or_city = shipping_details.address.city
-#                 profile.default_street_address1 = shipping_details.address.line1
-#                 profile.default_street_address2 = shipping_details.address.line2
-#                 profile.default_county = shipping_details.address.state
-#                 profile.save()
+        account = None
+        username = intent.metadata.username
+        if request.user.is_authenticated:
+            account = UserAccount.objects.get(user__username=username)
+            if save_info:
+                account.default_phone_number = shipping_details.phone
+                account.default_street_address1 = shipping_details.address.line1
+                account.default_street_address2 = shipping_details.address.line2
+                account.default_town_or_city = shipping_details.address.city
+                account.default_county = shipping_details.address.state
+                account.default_eircode = shipping_details.address.postal_code
+                account.save()
 
         order_exists = False
         attempt = 1
@@ -119,7 +118,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                         full_name = shipping_details.name,
-                        # user_profile=profile,
+                        user_account=account,
                         email = billing_details.email,
                         phone_number = shipping_details.phone,
                         street_address1 = shipping_details.address.line1,
