582ce1c4066d9149574bf0ac58900c0a9a99f44c
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-09-18 11:26:57 +0000

    Feat: Add AWS cache control.

--- gardening/settings.py
+++ gardening/settings.py
@@ -178,9 +178,15 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
+MEDIAFILES_DIRS = [os.path.join(BASE_DIR, 'media'), ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    
     AWS_STORAGE_BUCKET_NAME = 'gardening-services-e596b6371c3f'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
