fcd91edea06d9910d510dee05ea396253fd62ed0
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-09-19 09:56:02 +0000

    Fix: Remove whitespace breaking AWS_S3_CUSTOM_DOMAIN string.

--- gardening/settings.py
+++ gardening/settings.py
@@ -192,7 +192,7 @@ if 'USE_AWS' in os.environ:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.\
-                             {AWS_S3_REGION_NAME}.amazonaws.com'
+        {AWS_S3_REGION_NAME}.amazonaws.com'
 
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
