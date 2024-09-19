bc8f5db4d1a3c8c5bce88458190025da93705a3e
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-09-19 10:45:28 +0000

    Test: Update default_from_email for deployed site.

--- gardening/settings.py
+++ gardening/settings.py
@@ -115,7 +115,6 @@ SITE_ID = 1
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'info@thegardenpath.ie'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
@@ -123,7 +122,8 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    # DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+DEFAULT_FROM_EMAIL = 'info@thegardenpath.ie'
 
 
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
