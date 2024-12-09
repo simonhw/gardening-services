3cacd3b95f5a910b61908a179d9012f8fc5a8e57
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-12-09 17:42:23 +0000

    Fix: Remove unnecessary import of handler500.

--- gardening/urls.py
+++ gardening/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import handler500
 from home.views import custom_500_error
 
 urlpatterns = [
