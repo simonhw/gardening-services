9d0cbc70871d4d92a11ba9fe6462979b77efba4a
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-12-09 17:17:22 +0000

    Feat: Add custom view to preserve user authentication on 500 error page.

--- gardening/urls.py
+++ gardening/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler500
+from home.views import custom_500_error
 
 urlpatterns = [
     path("", include('home.urls'), name='home-urls'),
@@ -33,3 +35,7 @@ urlpatterns = [
     path("checkout/", include("checkout.urls")),
     path("contact_us/", include("contact.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# This line ensures that the custom view is used for 500 server errors
+# which has the correct user authentication status from the context.
+handler500 = custom_500_error
--- home/views.py
+++ home/views.py
@@ -16,3 +16,18 @@ def test_500_error_page(request):
     """
 
     raise Exception("Testing the 500 error page")
+
+
+def custom_500_error(request):
+    """
+    A custom view to handle internal server errors while also providing
+    the user authentication status in the context.
+    """
+
+    context = {
+        'user': getattr(request, 'user', None)
+    }
+
+    return render(
+        request, '500.html', context, status=500
+    )
